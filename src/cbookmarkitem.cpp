f1a528cdeeaad4c3aba51621bacd3481a5aa5741
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2014-09-05 00:11:12 +0700

    some realization of the mothods

--- src/cbookmarkitem.cpp
+++ src/cbookmarkitem.cpp
@@ -13,7 +13,50 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "cbookmarkitem.h"
+#include "cbookmarkmgr.h"
+#include "ctagitem.h"
 
-CBookmarkItem::CBookmarkItem()
+CBookmarkItem::CBookmarkItem(int id, const QString &title, CBookmarkMgr *mgr,
+        CBookmarkItem *parent)
+{
+    m_row = -1;
+    m_id = id;
+    m_title = title;
+    m_mgr = mgr;
+    m_parent = parent;
+}
+
+CBookmarkItem::~CBookmarkItem()
 {
 }
+
+void CBookmarkItem::setTitle(const QString &title)
+{
+    m_title = title;
+}
+
+void CBookmarkItem::addTag(CTagItem *tagItem)
+{
+    m_tagList.push_back(tagItem);
+}
+
+void CBookmarkItem::removeTag(CTagItem *tagItem)
+{
+    m_tagList.removeAll(tagItem);
+}
+
+void CBookmarkItem::add(CBookmarkItem *item)
+{
+    m_childList.push_back(item);
+}
+
+void CBookmarkItem::remove(int row)
+{
+    m_childList.removeAt(row);
+}
+
+void CBookmarkItem::setRow(int row)
+{
+    m_row = row;
+}
+
--- src/cbookmarkitem.h
+++ src/cbookmarkitem.h
@@ -15,10 +15,86 @@
 #ifndef CBOOKMARKITEM_H
 #define CBOOKMARKITEM_H
 
+#include <QString>
+#include <QList>
+class CTagItem;
+class CBookmarkMgr;
+
 class CBookmarkItem
 {
+    friend class CBookmarkMgr;
+private:
+    CBookmarkItem(int id, const QString &title, CBookmarkMgr *mgr,
+            CBookmarkItem *parent = 0);
 public:
-    CBookmarkItem();
+    ~CBookmarkItem();
+
+    inline CBookmarkItem *parent() const;
+    inline int row() const;
+    inline int id() const;
+
+    inline const QString &title() const;
+    void setTitle(const QString &title);
+
+    inline int tagCount() const;
+    inline CTagItem *tagAt(int i) const;
+    void addTag(CTagItem *tagItem);
+    void removeTag(CTagItem *tagItem);
+
+    inline int childCount() const;
+    inline CBookmarkItem *childAt(int row) const;
+    void add(CBookmarkItem *item);
+    void remove(int row);
+private:
+    void setRow(int row);
+private:
+    int m_row;
+    int m_id;
+    QString m_title;
+    CBookmarkMgr *m_mgr;
+    CBookmarkItem *m_parent;
+    QList<CBookmarkItem *> m_childList;
+    QList<CTagItem *> m_tagList;
 };
+
+CBookmarkItem *CBookmarkItem::parent() const
+{
+    return m_parent;
+}
+
+int CBookmarkItem::row() const
+{
+    return m_row;
+}
+
+int CBookmarkItem::id() const
+{
+    return m_id;
+}
+
+const QString &CBookmarkItem::title() const
+{
+    return m_title;
+}
+
+int CBookmarkItem::tagCount() const
+{
+    return m_tagList.count();
+}
+
+CTagItem *CBookmarkItem::tagAt(int i) const
+{
+    return m_tagList[i];
+}
+
+int CBookmarkItem::childCount() const
+{
+    return m_childList.count();
+}
+
+CBookmarkItem *CBookmarkItem::childAt(int row) const
+{
+    return m_childList[row];
+}
 
 #endif // CBOOKMARKITEM_H

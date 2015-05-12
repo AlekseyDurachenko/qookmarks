41fbd6d99aaff90fd29cb010282b38c80b066545
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2015-05-12 16:38:26 +0600

    add comments

--- src/widget/navigationview/cnavtagview.cpp
+++ src/widget/navigationview/cnavtagview.cpp
@@ -1,6 +1,19 @@
+// Copyright 2015, Durachenko Aleksey V. <durachenko.aleksey@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "cnavtagview.h"
 #include <QDragMoveEvent>
-#include <QDebug>
 
 
 CNavTagView::CNavTagView(QWidget *parent) : QTreeView(parent)
@@ -9,6 +22,7 @@ CNavTagView::CNavTagView(QWidget *parent) : QTreeView(parent)
 
 void CNavTagView::dropEvent(QDropEvent *event)
 {
+    // HACK: it can be incorrect usage of the dragAction modification
     if (event->keyboardModifiers() & Qt::ControlModifier)
         event->setDropAction(Qt::CopyAction);
     else
